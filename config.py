98bd13b60159c18a2fa73f58786268afb9add668
Vikas-841 <150503522+Vikas-841@users.noreply.github.com>
2024-01-11 16:38:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,8 +61,6 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 # Ask Doubt on telegram @KingVJ01
 
 # File Caption Information
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
 # Enable - True or Disable - False
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
